81382e05259621087a36667706b7ab2da149a77c
Manel <manelhammouche3@gmail.com>
2022-06-20 18:08:13 +0100

    Add navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Categories from './components/Categories/Categories';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
