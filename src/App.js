7113aa4ae9cc2c9f517cfb3b6bb8c2231fc27a71
Manel <manelhammouche3@gmail.com>
2022-06-21 14:04:22 +0100

    Refactor api call

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import Navbar from './components/Navbar/Navbar';
 import Categories from './components/Categories/Categories';
+import { fetchLeagues, loadLeagues } from './redux/leagues/leagues';
 
 function App() {
+  const dispatch = useDispatch();
+  const leagues = useSelector((state) => state.leagues);
+
+  const intitData = async () => {
+    const result = await fetchLeagues();
+    dispatch(loadLeagues(result));
+  };
+
+  if (leagues.length < 1) {
+    intitData();
+  }
+
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Categories />} />
-      </Routes>
+      {leagues.length > 1 ? (
+        <Routes>
+          <Route path="/" element={<Categories />} />
+        </Routes>
+      ) : ('Loading')}
     </div>
   );
 }
--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,35 +1,38 @@
-// import styles from './Categories.module.css';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchLeagues } from '../../redux/leagues/leagues';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+// import { fetchTeams, loadTeams } from '../../redux/teams/teams';
+// import Team from '../Team/Team';
 
 function Categories() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchLeagues);
-  }, []);
-
+  // const dispatch = useDispatch();
   const leagues = useSelector((state) => state.leagues);
-  console.log(leagues);
-
-  const [league, setLeague] = useState();
-  console.log(league);
+  // const teams = useSelector((state) => state.teams);
+  const [league, setLeague] = useState(leagues.filter((l) => l.league_id === 39)[0]);
 
   const displayLeague = (id) => {
-    setLeague(() => [...leagues.filter((l) => l.league_id === id)]);
+    setLeague(() => leagues.filter((l) => l.league_id === id)[0]);
+    // const result = await fetchTeams(id, 2021);
+    // dispatch(loadTeams(result));
   };
 
   return (
     <div>
       <div>
-        <button type="button" onClick={() => displayLeague(39)}>Premier League</button>
-        <button type="button" onClick={() => displayLeague(135)}>Serie A</button>
-        <button type="button" onClick={() => displayLeague(140)}>La Liga</button>
-        <button type="button" onClick={() => displayLeague(186)}>Ligue 1</button>
+        <button type="button" onClick={() => displayLeague(39)}>
+          Premier League
+        </button>
+        <button type="button" onClick={() => displayLeague(135)}>
+          Serie A
+        </button>
+        <button type="button" onClick={() => displayLeague(140)}>
+          La Liga
+        </button>
+        <button type="button" onClick={() => displayLeague(186)}>
+          Ligue 1
+        </button>
       </div>
 
-      {/* <div>
+      <div>
         <div>
           <img src={league.league_logo} alt="league logo" />
         </div>
@@ -37,6 +40,12 @@ function Categories() {
           <h3>{league.league_name}</h3>
           <p>{league.league_country}</p>
         </div>
+      </div>
+      {/*
+      <div>
+        {teams.length > 1 && teams.map((team) => (
+          <Team key={team.team_id} team={team} />
+        ))}
       </div> */}
     </div>
   );
