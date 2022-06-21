dc08b83052bba93a671a25a07f04a2fecdd1bc5d
Manel <manelhammouche3@gmail.com>
2022-06-21 11:44:33 +0100

    Filter leagues

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,24 +1,43 @@
 // import styles from './Categories.module.css';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchLeagues } from '../../redux/leagues/leagues';
 
 function Categories() {
-  // const leagues = useSelector((state) => state.leagues);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchLeagues);
   }, []);
 
+  const leagues = useSelector((state) => state.leagues);
+  console.log(leagues);
+
+  const [league, setLeague] = useState();
+  console.log(league);
+
+  const displayLeague = (id) => {
+    setLeague(() => [...leagues.filter((l) => l.league_id === id)]);
+  };
+
   return (
     <div>
       <div>
-        <button type="button">Premier League</button>
-        <button type="button">Serie A</button>
-        <button type="button">La Liga</button>
-        <button type="button">Ligue 1</button>
+        <button type="button" onClick={() => displayLeague(39)}>Premier League</button>
+        <button type="button" onClick={() => displayLeague(135)}>Serie A</button>
+        <button type="button" onClick={() => displayLeague(140)}>La Liga</button>
+        <button type="button" onClick={() => displayLeague(186)}>Ligue 1</button>
       </div>
+
+      {/* <div>
+        <div>
+          <img src={league.league_logo} alt="league logo" />
+        </div>
+        <div>
+          <h3>{league.league_name}</h3>
+          <p>{league.league_country}</p>
+        </div>
+      </div> */}
     </div>
   );
 }
