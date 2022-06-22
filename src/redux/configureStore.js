386f8ae883480333986ecca73557d3d8ea369c05
Manel <manelhammouche3@gmail.com>
2022-06-22 12:04:38 +0100

    Remove teams reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import leaguesReducer from './leagues/leagues';
-import teamsReducer from './teams/teams';
 
 const rootReducer = combineReducers({
   leagues: leaguesReducer,
-  teams: teamsReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
