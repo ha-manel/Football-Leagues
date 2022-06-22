2811f905f66f4739f231f3c1c4439e8f379d18bf
Manel <manelhammouche3@gmail.com>
2022-06-22 12:03:52 +0100

    Create team component

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,7 +1,36 @@
-// import styles from './Team.module.css';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import styles from './Team.module.css';
 
-function Team() {
-  return <div>Team</div>;
+function Team({ team }) {
+  return (
+    <Link to="/team" state={{ team }}>
+      <div className={styles.teamContainer}>
+        <i className={`${styles.teamDetailsIcon} fa-solid fa-right-from-bracket`} />
+        <div className={styles.teamImgContainer}>
+          <img className={styles.teamImg} src={team.logo} alt="team logo" />
+        </div>
+        <div className={styles.teamInfo}>
+          <h3 className={styles.teamName}>{team.name}</h3>
+          <p className={styles.teamCity}>{team.city}</p>
+        </div>
+      </div>
+    </Link>
+  );
 }
+
+Team.propTypes = {
+  team: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    founded: PropTypes.number.isRequired,
+    logo: PropTypes.string.isRequired,
+  }),
+};
+
+Team.defaultProps = {
+  team: {},
+};
 
 export default Team;
